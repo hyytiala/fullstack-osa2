b06720fbca84be0c3e0c649d815f3a595b52bfee
hyytiala <otto.hyytiala@gmail.com>
2018-03-02 17:18:37 +0200

    kirjautuminen

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,108 +1,195 @@
 import React from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            notes: [],
-            newNote: '',
-            showAll: true
+  constructor() {
+    super()
+    this.state = {
+      notes: [],
+      newNote: '',
+      showAll: true,
+      error: null,
+      username: '',
+      password: '',
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    noteService.getAll().then(notes =>
+      this.setState({ notes })
+    )
+
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      this.setState({ user })
+      noteService.setToken(user.token)
+    }
+  }
+
+  toggleVisible = () => {
+    this.setState({ showAll: !this.state.showAll })
+  }
+
+  addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: this.state.newNote,
+      date: new Date(),
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(newNote => {
+        this.setState({
+          notes: this.state.notes.concat(newNote),
+          newNote: ''
+        })
+      })
+  }
+
+  toggleImportanceOf = (id) => {
+    return () => {
+      const note = this.state.notes.find(n => n.id === id)
+      const changedNote = { ...note, important: !note.important }
+
+      noteService
+        .update(id, changedNote)
+        .then(changedNote => {
+          this.setState({
+            notes: this.state.notes.map(note => note.id !== id ? note : changedNote)
+          })
+        })
+        .catch(error => {
+          this.setState({
+            error: `muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`,
+            notes: this.state.notes.filter(n => n.id !== id)
+          })
+          setTimeout(() => {
+            this.setState({ error: null })
+          }, 50000)
+        })
+    }
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
+      noteService.setToken(user.token)
+      this.setState({ username: '', password: '', user })
+    } catch (exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleNoteChange = (event) => {
+    this.setState({ newNote: event.target.value })
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  toggleVisible = () => {
+    this.setState({ showAll: !this.state.showAll })
+  }
+
+  render() {
+    const notesToShow =
+      this.state.showAll ?
+        this.state.notes :
+        this.state.notes.filter(note => note.important === true)
+
+    const label = this.state.showAll ? 'vain tärkeät' : 'kaikki'
+
+    const loginForm = () => (
+      <div>
+        <h2>Kirjaudu</h2>
+
+        <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button>kirjaudu</button>
+        </form>
+      </div>
+    )
+
+    const noteForm = () => (
+      <div>
+        <h2>Luo uusi muistiinpano</h2>
+
+        <form onSubmit={this.addNote}>
+          <input
+            value={this.state.newNote}
+            onChange={this.handleNoteChange}
+          />
+          <button>tallenna</button>
+        </form>
+      </div>
+    )
+
+    return (
+      <div>
+        <h1>Muistiinpanot</h1>
+
+        <Notification message={this.state.error} />
+
+        {this.state.user === null ?
+          loginForm() :
+          <div>
+            <p>{this.state.user.name} logged in</p>
+            {noteForm()}
+          </div>
         }
-        console.log('constructor')
-    }
 
-    componentDidMount() {
-        console.log('will mount')
-        noteService
-            .getAll()
-            .then(notes => {
-                this.setState({ notes })
-            })
-    }
+        <div>
+          <button onClick={this.toggleVisible}>
+            näytä {label}
+          </button>
+        </div>
+        <ul>
+          {notesToShow.map(note =>
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={this.toggleImportanceOf(note.id)}
+            />)}
+        </ul>
 
-    addNote = (event) => {
-        event.preventDefault()
-        const noteObject = {
-            content: this.state.newNote,
-            date: new Date(),
-            important: Math.random() > 0.5
-        }
-
-        noteService
-            .create(noteObject)
-            .then(newNote => {
-                this.setState({
-                    notes: this.state.notes.concat(newNote),
-                    newNote: ''
-                })
-            })
-    }
-
-    handleNoteChange = (event) => {
-        console.log(event.target.value)
-        this.setState({ newNote: event.target.value })
-    }
-
-    toggleVisible = () => {
-        this.setState({ showAll: !this.state.showAll })
-    }
-
-    toggleImportanceOf = (id) => {
-        return () => {
-            const note = this.state.notes.find(n => n.id === id)
-            const changedNote = { ...note, important: !note.important }
-
-            noteService
-                .update(id, changedNote)
-                .then(changedNote => {
-                    const notes = this.state.notes.filter(n => n.id !== id)
-                    this.setState({
-                        notes: notes.concat(changedNote)
-                    })
-                })
-                .catch(error => {
-                    alert(`muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`)
-                    this.setState({ notes: this.state.notes.filter(n => n.id !== id) })
-                })
-        }
-    }
-
-
-
-    render() {
-        console.log('render')
-        const notesToShow =
-            this.state.showAll ?
-                this.state.notes :
-                this.state.notes.filter(note => note.important === true)
-
-        const label = this.state.showAll ? 'vain tärkeät' : 'kaikki'
-
-        return (
-            <div>
-                <h1>Muistiinpanot</h1>
-                <div>
-                    <button onClick={this.toggleVisible}>
-                        näytä {label}
-                    </button>
-                </div>
-                <ul>
-                    {notesToShow.map(note =>
-                        <Note
-                            key={note.id}
-                            note={note}
-                            toggleImportance={this.toggleImportanceOf(note.id)}
-                        />)}
-                </ul>
-                <form onSubmit={this.addNote}>
-                    <input value={this.state.newNote} onChange={this.handleNoteChange} />
-                    <button type="submit">tallenna</button>
-                </form>
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default App
